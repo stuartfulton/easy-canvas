d8fa9bafc73c8dbe36792b3e28264a960288d226
stuartfulton <w.stuart.fulton@gmail.com>
2015-07-03 01:05:55 -0500

    Update easy-image.js

--- easy-image.js
+++ easy-image.js
@@ -1,12 +1,15 @@
-function EasyImage(width, height, graphicsContext) {
-	this.image = graphicsContext.createImageData(width, height);
-	this.pixelData = this.image.data;
-	
-	this.setPixel = function (x, y, r, g, b, a) {
-		var base = ((y * width) + x) * 4;
+var EasyImage = (function(window, undefined){
+	var _EasyImage = function(width, height, graphicsContext){
+		this.image = graphicsContext.createImageData(width, height);
+		this.pixelData = this.image.data;
+	}
+	_EasyImage.prototype.setPixel = function (x, y, r, g, b, a) {
+		var base = ((y * this.width) + x) * 4;
 		this.pixelData[base] = r;
 		this.pixelData[base + 1] = g;
 		this.pixelData[base + 2] = b;
 		this.pixelData[base + 3] = a;
 	}
-}
+	return _EasyImage;
+})();
+
