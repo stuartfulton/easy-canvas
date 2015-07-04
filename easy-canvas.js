5066f22c31fc41f3116be40e0ebf6bcc9c178f4b
stuartfulton <w.stuart.fulton@gmail.com>
2015-07-04 15:58:01 -0500

    Update easy-canvas.js

--- easy-canvas.js
+++ easy-canvas.js
@@ -4,43 +4,7 @@ var EasyCanvas = (function(window, undefined){
 		this.g = canvas.getContext('2d');
 		this.width = canvas.width;
 		this.height = canvas.height;
-		this.image = g.createImageData(width, height);
-	}
-	
-	_EasyImage.prototype.setPixel = function (x, y, Color) {
-		var base = ((y * this.width) + x) * 4;
-		this.image.data[base] = Color.r;
-		this.image.data[base + 1] = Color.g;
-		this.image.data[base + 2] = Color.b;
-		this.image.data[base + 3] = Color.a;
-	};
-	
-	_EasyImage.prototype.Color = function(r, g, b, a) {
-		this.r = r || 0;
-		this.g = g || 0;
-		this.b = b || 0;
-		this.a = a || 255;
-	};
-	
-	_EasyImage.prototype.rect = function(x1, y1, width, height, color){
-		var x2 = x1 + width;
-		var y2 = y1 + height;
-		for (var x = x1; x <= x2; x++){
-			for (var y = y1; y <= y2; y++){
-				this.setPixel(x, y, color);
-			}
-		}
-	}
-
-	return _EasyImage;
-})();
-
-var g, WIDTH, HEIGHT;
-var EasyImage = (function(window, undefined){
-	var _EasyImage = function(width, height, graphicsContext){
-		this.width = width;
-		this.height = height;
-		this.image = graphicsContext.createImageData(width, height);
+		this.image = this.g.createImageData(this.width, this.height);
 	}
 	
 	_EasyImage.prototype.setPixel = function (x, y, Color) {
@@ -75,10 +39,8 @@ var EasyImage = (function(window, undefined){
 	return _EasyImage;
 })();
 
-var ezi = new EasyImage('canvas');
+var ezi = new EasyCanvas('canvas');
 
 ezi.rect(10, 50, 10, 40, new ezi.Color(255, 0, 0));
 ezi.rect(0, 70, 80, 10, new ezi.Color(0, 0, 255));
 ezi.draw(0, 0);
-
-g.putImageData(ezi.image, 0, 0);
