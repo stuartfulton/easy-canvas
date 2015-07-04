8c15ec868bf82a48bfa49b92faa2d158baa2a0cd
stuartfulton <w.stuart.fulton@gmail.com>
2015-07-04 01:52:18 -0500

    Expanded easyImage; added example

--- easy-image.js
+++ easy-image.js
@@ -1,17 +1,47 @@
+var Color = function(r, g, b, a) {
+    this.r = r || 0;
+    this.g = g || 0;
+    this.b = b || 0;
+    this.a = a || 255;
+};
+
 var EasyImage = (function(window, undefined){
 	var _EasyImage = function(width, height, graphicsContext){
         this.width = width;
         this.height = height;
 		this.image = graphicsContext.createImageData(width, height);
-		this.pixelData = this.image.data;
 	}
-	_EasyImage.prototype.setPixel = function (x, y, r, g, b, a) {
+    
+	_EasyImage.prototype.setPixel = function (x, y, Color) {
 		var base = ((y * this.width) + x) * 4;
-		this.pixelData[base] = r;
-		this.pixelData[base + 1] = g;
-		this.pixelData[base + 2] = b;
-		this.pixelData[base + 3] = a;
-	}
+		this.image.data[base] = Color.r;
+		this.image.data[base + 1] = Color.g;
+		this.image.data[base + 2] = Color.b;
+		this.image.data[base + 3] = Color.a;
+	};
+    
+    _EasyImage.prototype.rect = function(x1, y1, width, height, color){
+        var x2 = x1 + width;
+        var y2 = y1 + height;
+        for (var x = x1; x <= x2; x++){
+            for (var y = y1; y <= y2; y++){
+                this.setPixel(x, y, color);
+            }
+        }
+    }
+    
 	return _EasyImage;
 })();
 
+var g, WIDTH, HEIGHT;
+var canvas = document.getElementById('canvas');
+g = canvas.getContext('2d');
+WIDTH = canvas.width;
+HEIGHT = canvas.height;
+
+var ezi = new EasyImage(WIDTH, HEIGHT, g);
+
+ezi.rect(10, 50, 10, 40, new Color(255, 0, 0));
+ezi.rect(0, 70, 80, 10, new Color(0, 0, 255));
+
+g.putImageData(ezi.image, 0, 0);
